471f74263e46c4ce0855b45fd634aa7cf0668b7e
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-05-10 11:16:42 -0300

    test-push

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -51,7 +51,7 @@ class HomePage extends StatelessWidget {
                     c.nome,
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 24),
+                    style: TextStyle(fontSize: 25),
                   ),
                   Text(
                     'descrição...',
